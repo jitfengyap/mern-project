463f92c98d4cd3913ed255a517195cded0e49868
Yap Jit Feng <jitfeng@gmail.com>
2024-06-19 18:35:43 +0800

    setup phase 2 completed

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  ShoppingCartOutlined,
+  HomeOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutline,
+} from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
+import profileImage from "assets/profile.jpeg";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
